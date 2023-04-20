65675d6efffc0edb812d30a9af4297f6bb963eed
Kat <kmunson13@gmail.com>
2023-04-20 16:05:39 -0400

    Add book class and set up book routes with Blueprint, jsonify

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,40 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
-hello_world_bp = Blueprint("hello_world", __name__)
+# hello_world_bp = Blueprint("hello_world", __name__)
+books_bp = Blueprint("books", __name__, url_prefix="/books")
 
-@hello_world_bp.route("/hello-world", methods=["GET"])
+# @hello_world_bp.route("/hello-world", methods=["GET"])
 
-def say_hello_world():
-    my_beautiful_world = "Hello world!"
-    return my_beautiful_world
+# def say_hello_world():
+#     my_beautiful_world = "Hello world!"
+#     return my_beautiful_world
 
-@hello_world_bp.route("/hello/JSON", methods=['GET'])
+# @hello_world_bp.route("/hello/JSON", methods=['GET'])
 
-def say_hello_json():
-    return {
-        "name": "Ada Lovelace",
-        "message": "Hello!",
-        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
-    }
+# def say_hello_json():
+#     return {
+#         "name": "Ada Lovelace",
+#         "message": "Hello!",
+#         "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
+#     }
+    
+class Book:
+    def __init__(self, id, title, description):
+        self.id = id
+        self.title = title
+        self.description = description
+        
+books = [Book(1, "Made Up Book", "Most excellent review and description."),
+         Book(2, "Great Book", "A Book on greatness, by the greatest"),
+         Book(3, "Best of books, worst of books", "It was the best of books and the worst of books.")]
+
+@books_bp.route("", methods=["GET"])
+def handle_books():
+    books_response = []
+    for book in books:
+        books_response.append({
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+        })
+    return jsonify(books_response)
