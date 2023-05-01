1b164b9a4ec2e95393a20d82370592aa2daa340b
Kat <kmunson13@gmail.com>
2023-05-01 14:29:55 -0400

    Add update, read one item and delete routes

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,7 @@
 # from flask import Blueprint, jsonify, abort, make_response
-from flask import request
 from app import db
 from app.models.book import Book
-from flask import Blueprint, jsonify, make_response, request
+from flask import Blueprint, jsonify, make_response, request, query
 
 
 
@@ -34,18 +33,19 @@ books_bp = Blueprint("books", __name__, url_prefix="/books")
 #          Book(2, "Great Book", "A Book on greatness, by the greatest"),
 #          Book(3, "Best of books, worst of books", "It was the best of books and the worst of books.")]
 
-# def validate_book(book_id):
-#     try:
-#         book_id = int(book_id)
-#     except:
-#         abort(make_response({"message":f"book {book_id} invalid"}, 400))
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({"message":f"book {book_id} invalid"}, 400))
 
-#     for book in books:
-#         if book.id == book_id:
-#             return book
+    book = Book.query.get(book_id)
 
-#     abort(make_response({"message":f"book {book_id} not found"}, 404))
+    if not book:
+        abort(make_response({"message":f"book {book_id} not found"}, 404))
 
+    return book
+        
 # @books_bp.route("", methods=["GET"])
 # def handle_books():
 #     books_response = []
@@ -57,14 +57,14 @@ books_bp = Blueprint("books", __name__, url_prefix="/books")
 #         })
 #     return jsonify(books_response)
 
-# @books_bp.route("/<book_id>", methods=["GET"])
-# def handle_book(book_id):
-#     book = validate_book(book_id)
-#     return {
-#                 "id": book.id,
-#                 "title": book.title,
-#                 "description": book.description
-#             }
+@books_bp.route("/<book_id>", methods=["GET"])
+def read_one_book(book_id):
+    book = validate_book(book_id)
+    return {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            }
 
 @books_bp.route('', methods=["GET"])
 def read_all_books():
@@ -87,3 +87,25 @@ def create_book():
     db.session.commit()
     
     return make_response(f'Book {new_book.title} successfully created', 201)
+
+@books_bp.route("/<book_id>", methods=["PUT"])
+def update_book(book_id):
+    book = validate_book(book_id)
+    
+    request_body = request.get_json()
+    
+    book.title = request_body["title"]
+    book.description = request_body["description"]
+    
+    db.session.commit()
+    
+    return make_response(f"Book {book.id} successfully updated")
+
+@books_bp.route("/<book_id>", methods=["DELETE"])
+def delete_book(book_id):
+    book = validate_book(book_id)
+    
+    db.session.delete(book)
+    db.session.commit()
+    
+    return make_response(f"Book #{book.id} successfully deleted")
